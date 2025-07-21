26d9b0fc9db51cc23596b3eafb8b6d765112d45f
Md. Emon Miah <umayermdemon@gmail.com>
2025-07-21 17:49:56 +0600

    feat: add origin on cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,8 @@ import cors from "cors";
 import router from "./app/modules/event/event.route";
 const app = express();
 
-app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://quick-sched.vercel.app" }));
 app.use(express.json());
 
 app.use("/events", router);
